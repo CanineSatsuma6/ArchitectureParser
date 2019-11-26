2d410a8c497d6ee28daafd1663f73c741c6c20bd
Collin McIntyre <crm11396@gmail.com>
2019-11-26 11:33:58 -0600

    Added IConnectable interface and skeleton Connection class

---
+++ ArchitectureParser/Architecture/Connections/IConnectable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ArchitectureParser.Architecture.Connections
+{
+    public interface IConnectable
+    {
+        // Properties
+        string           Name        { get; }
+        ISet<Connection> Connections { get; }
+
+        // Methods
+        Connection Connect(IConnectable destination);
+    }
+}
