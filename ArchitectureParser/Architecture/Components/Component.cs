8859f4f43db9a1e6c56ccb50b155f3627df52766
Collin McIntyre <crm11396@gmail.com>
2019-11-26 12:07:17 -0600

    Added implementations for Component and Connection

---
+++ ArchitectureParser/Architecture/Components/Component.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+using ArchitectureParser.Architecture.Connections;
+
+namespace ArchitectureParser.Architecture.Components
+{
+    public class Component : IConnectable
+    {
+        // Private backing field for the Connections property
+        private HashSet<Connection> m_connections;
+
+        // Property for the component name. Required by IConnectable
+        public string Name 
+        { 
+            get; 
+        }
+
+        // Property for the component connections. Required by IConnectable
+        public ISet<Connection> Connections 
+        { 
+            get { return m_connections; }
+        }
+
+        // The constructor creates a component with the given name
+        public Component(string name)
+        {
+            Name          = name;
+            m_connections = new HashSet<Connection>();
+        }
+
+        // Creates and returns a connection with this component as the source
+        // and the provided IConnectable as the destination
+        public Connection Connect(IConnectable destination)
+        {
+            var connection = new Connection(this, destination);
+
+            Connections.Add(connection);
+            destination.Connections.Add(connection);
+
+            return connection;
+        }
+    }
+}
--- ArchitectureParser/Architecture/Connections/Connection.cs
+++ ArchitectureParser/Architecture/Connections/Connection.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ArchitectureParser.Architecture.Connections
+﻿namespace ArchitectureParser.Architecture.Connections
 {
     public class Connection
     {
+        // The source of the connection
+        public IConnectable Source 
+        { 
+            get; 
+            set; 
+        }
+
+        // The destination of the connection
+        public IConnectable Destination 
+        { 
+            get; 
+            set; 
+        }
+
+        // The constructor creates a Connection from the source to the destination
+        public Connection(IConnectable source = null, IConnectable destination = null)
+        {
+            Source      = source;
+            Destination = destination;
+        }
     }
 }
