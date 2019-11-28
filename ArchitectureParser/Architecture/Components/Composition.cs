583595c7cb50abf2b7ab4c4cb19116e3b7f6c14a
Collin McIntyre <crm11396@gmail.com>
2019-11-28 00:09:10 -0600

    Implemented Compositions

---
+++ ArchitectureParser/Architecture/Components/Composition.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using ArchitectureParser.Architecture.Connections;
+
+namespace ArchitectureParser.Architecture.Components
+{
+    public class Composition : IConnectable
+    {
+        private HashSet<Connection>   m_connections;
+        private HashSet<IConnectable> m_contents;
+
+        public string Name
+        {
+            get;
+        }
+
+        public ISet<Connection> Connections
+        {
+            get { return m_connections; }
+        }
+
+        public ISet<IConnectable> Contents
+        {
+            get { return m_contents; }
+        }
+
+        public Composition(string name)
+        {
+            Name          = name;
+            m_connections = new HashSet<Connection>();
+            m_contents    = new HashSet<IConnectable>();
+        }
+
+        public Connection Connect(IConnectable destination, string output, string input)
+        {
+            var connection = new Connection(this, output, destination, input);
+
+            Connections.Add(connection);
+            destination.Connections.Add(connection);
+
+            return connection;
+        }
+
+        // This method effectively removes the composition from the component tree.
+        // It's intended for this Composition to be discarded shortly after calling this method
+        public void ConsolidateConnections()
+        {
+            // Ensure components consuming composition inputs/outputs have a source
+
+            // Find connections that are consumed by internal components but are not provided by external components
+            var neglectedInternalConnections = from c in Connections where c.Source == this && Contents.Contains(c.Destination) && !Connections.Any(con => con.DestinationInput == c.SourceOutput) select c;
+
+            if (neglectedInternalConnections.Count() > 0)
+            {
+                throw new NullReferenceException(string.Format("These inputs on composition \"{0}\" are not provided externally: {1}", Name, string.Join(", ", from c in neglectedInternalConnections select c.SourceOutput)));
+            }
+
+            // Find connections that are consumed by external components but are not provided by internal components
+            var neglectedExternalConnections = from c in Connections where c.Source == this && !Contents.Contains(c.Destination) && c.Destination != this && !Connections.Any(con => con.DestinationInput == c.SourceOutput) select c;
+
+            if (neglectedExternalConnections.Count() > 0)
+            {
+                throw new NullReferenceException(string.Format("These outputs on composition \"{0}\" are not provided internally: {1}", Name, string.Join(", ", from c in neglectedExternalConnections select c.SourceOutput)));
+            }
+
+            // Begin consolidating composition connections
+
+            // First, consolidate the connections leading into the composition
+            var incomingConnections = from c in Connections where c.Destination == this && !Contents.Contains(c.Source) && c.Source != this select c;
+
+            foreach (var connection in incomingConnections)
+            {
+                // Find all of the places this input is consumed
+                var destinationConnections = from c in Connections where c.Source == this && c.SourceOutput == connection.DestinationInput select c;
+
+                foreach (var destinationConnection in destinationConnections)
+                {
+                    // If the input passes through straight to an output, connect the input to the external components
+                    if (destinationConnection.Destination == this)
+                    {
+                        var externalDestinationConnections = from c in Connections where c.Source == this && c.SourceOutput == destinationConnection.DestinationInput select c;
+
+                        foreach (var externalDestinationConnection in externalDestinationConnections)
+                        {
+                            connection.Source.Connect(externalDestinationConnection.Destination, connection.SourceOutput, externalDestinationConnection.DestinationInput);
+                            externalDestinationConnection.Destination.Connections.Remove(externalDestinationConnection);
+                        }
+                    }
+
+                    // If the input connects to a component, connect the input to that component
+                    else
+                    {
+                        connection.Source.Connect(destinationConnection.Destination, connection.SourceOutput, destinationConnection.DestinationInput);
+                        destinationConnection.Destination.Connections.Remove(destinationConnection);
+                    }
+                }
+
+                connection.Source.Connections.Remove(connection);
+            }
+
+            // Then, consolidate the connections leading out of the composition
+            var outgoingConnections = from c in Connections where c.Destination == this && Contents.Contains(c.Source) select c;
+
+            foreach (var connection in outgoingConnections)
+            {
+                var destinationConnections = from c in Connections where c.Source == this && c.SourceOutput == connection.DestinationInput select c;
+
+                foreach (var destinationConnection in destinationConnections)
+                {
+                    connection.Source.Connect(destinationConnection.Destination, connection.SourceOutput, destinationConnection.DestinationInput);
+                    destinationConnection.Destination.Connections.Remove(destinationConnection);
+                }
+
+                connection.Source.Connections.Remove(connection);
+            }
+
+            // Remove all connections and content from the composition
+            Connections.Clear();
+            Contents.Clear();
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ ArchitectureParserTest/CompositionTest.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+using ArchitectureParser.Architecture.Components;
+
+namespace ArchitectureParserTest
+{
+    [TestClass]
+    public class CompositionTest
+    {
+        [TestMethod]
+        public void CompositionConstructor()
+        {
+            var composition = new Composition("Composition");
+
+            Assert.AreEqual("Composition", composition.Name);
+            Assert.AreEqual(0,             composition.Connections.Count);
+            Assert.AreEqual(0,             composition.Contents.Count);
+        }
+
+        [TestMethod]
+        public void CompositionConnect()
+        {
+            var composition = new Composition("Composition");
+            var externalComponent = new Component("Component");
+
+            var connection = composition.Connect(externalComponent, "Output", "Input");
+
+            Assert.AreEqual(composition, connection.Source);
+            Assert.AreEqual(externalComponent, connection.Destination);
+            Assert.AreEqual(1, composition.Connections.Count);
+            Assert.AreEqual(1, externalComponent.Connections.Count);
+            Assert.AreEqual(0, composition.Contents.Count);
+            Assert.IsTrue(composition.Connections.Contains(connection));
+            Assert.IsTrue(externalComponent.Connections.Contains(connection));
+        }
+
+        [TestMethod]
+        public void CompositionConsolidateExternalToInternal()
+        {
+            var externalComponent = new Component("External");
+            var internalComponent = new Component("Internal");
+            var composition       = new Composition("Composition");
+
+            composition.Contents.Add(internalComponent);
+
+            externalComponent.Connect(composition, "Output", "CompositionInput");
+            composition.Connect(internalComponent, "CompositionInput", "Input");
+
+            composition.ConsolidateConnections();
+
+            Assert.AreEqual(1, externalComponent.Connections.Count);
+            Assert.AreEqual(1, internalComponent.Connections.Count);
+            Assert.AreEqual(0, composition.Connections.Count);
+            Assert.AreEqual(0, composition.Contents.Count);
+
+            var connection = externalComponent.Connections.First();
+
+            Assert.AreEqual(externalComponent, connection.Source);
+            Assert.AreEqual(internalComponent, connection.Destination);
+            Assert.AreEqual("Output", connection.SourceOutput);
+            Assert.AreEqual("Input", connection.DestinationInput);
+            Assert.IsTrue(externalComponent.Connections.Contains(connection));
+            Assert.IsTrue(internalComponent.Connections.Contains(connection));
+        }
+
+        [TestMethod]
+        public void CompositionConsolidateInternalToExternal()
+        {
+            var externalComponent = new Component("External");
+            var internalComponent = new Component("Internal");
+            var composition       = new Composition("Composition");
+
+            composition.Contents.Add(internalComponent);
+
+            internalComponent.Connect(composition, "Output", "CompositionOutput");
+            composition.Connect(externalComponent, "CompositionOutput", "Input");
+
+            composition.ConsolidateConnections();
+
+            Assert.AreEqual(1, externalComponent.Connections.Count);
+            Assert.AreEqual(1, internalComponent.Connections.Count);
+            Assert.AreEqual(0, composition.Connections.Count);
+            Assert.AreEqual(0, composition.Contents.Count);
+
+            var connection = internalComponent.Connections.First();
+
+            Assert.AreEqual(externalComponent, connection.Destination);
+            Assert.AreEqual(internalComponent, connection.Source);
+            Assert.AreEqual("Output", connection.SourceOutput);
+            Assert.AreEqual("Input", connection.DestinationInput);
+            Assert.IsTrue(externalComponent.Connections.Contains(connection));
+            Assert.IsTrue(internalComponent.Connections.Contains(connection));
+        }
+
+        [TestMethod]
+        public void CompositionConsolidateChain()
+        {
+            var externalBefore    = new Component("Before");
+            var internalComponent = new Component("Internal");
+            var externalAfter     = new Component("After");
+            var composition       = new Composition("Composition");
+
+            composition.Contents.Add(internalComponent);
+
+            externalBefore.Connect(composition, "Output", "CompositionInput");
+            composition.Connect(internalComponent, "CompositionInput", "Input");
+
+            internalComponent.Connect(composition, "Output", "CompositionOutput");
+            composition.Connect(externalAfter, "CompositionOutput", "Input");
+
+            composition.ConsolidateConnections();
+
+            Assert.AreEqual(1, externalBefore.Connections.Count);
+            Assert.AreEqual(2, internalComponent.Connections.Count);
+            Assert.AreEqual(1, externalAfter.Connections.Count);
+            Assert.AreEqual(0, composition.Connections.Count);
+            Assert.AreEqual(0, composition.Contents.Count);
+
+            var connectionBefore = externalBefore.Connections.First();
+            var connectionAfter  = externalAfter.Connections.First();
+
+            Assert.AreEqual(internalComponent, connectionBefore.Destination);
+            Assert.AreEqual(externalBefore, connectionBefore.Source);
+            Assert.AreEqual("Output", connectionBefore.SourceOutput);
+            Assert.AreEqual("Input", connectionBefore.DestinationInput);
+            Assert.IsTrue(externalBefore.Connections.Contains(connectionBefore));
+            Assert.IsTrue(internalComponent.Connections.Contains(connectionBefore));
+
+            Assert.AreEqual(externalAfter, connectionAfter.Destination);
+            Assert.AreEqual(internalComponent, connectionAfter.Source);
+            Assert.AreEqual("Output", connectionAfter.SourceOutput);
+            Assert.AreEqual("Input", connectionAfter.DestinationInput);
+            Assert.IsTrue(externalAfter.Connections.Contains(connectionAfter));
+            Assert.IsTrue(internalComponent.Connections.Contains(connectionAfter));
+        }
+
+        [TestMethod]
+        public void CompositionConsolidatePassThrough()
+        {
+            var externalBefore = new Component("Before");
+            var externalAfter  = new Component("After");
+            var composition    = new Composition("Composition");
+
+            externalBefore.Connect(composition, "Output", "CompositionInput");
+            composition.Connect(composition, "CompositionInput", "CompositionOutput");
+            composition.Connect(externalAfter, "CompositionOutput", "Input");
+
+            composition.ConsolidateConnections();
+
+            Assert.AreEqual(1, externalBefore.Connections.Count);
+            Assert.AreEqual(1, externalAfter.Connections.Count);
+            Assert.AreEqual(0, composition.Connections.Count);
+            Assert.AreEqual(0, composition.Contents.Count);
+
+            var connection = externalBefore.Connections.First();
+
+            Assert.AreEqual(externalBefore, connection.Source);
+            Assert.AreEqual(externalAfter, connection.Destination);
+            Assert.AreEqual("Output", connection.SourceOutput);
+            Assert.AreEqual("Input", connection.DestinationInput);
+            Assert.IsTrue(externalBefore.Connections.Contains(connection));
+            Assert.IsTrue(externalAfter.Connections.Contains(connection));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void CompositionNoExternalInputProvider()
+        {
+            var internalComponent = new Component("Internal");
+            var composition       = new Composition("Composition");
+
+            composition.Contents.Add(internalComponent);
+            composition.Connect(internalComponent, "CompositionInput", "Input");
+
+            composition.ConsolidateConnections();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void CompositionNoInternalInputProvider()
+        {
+            var externalComponent = new Component("External");
+            var composition       = new Composition("Composition");
+
+            composition.Connect(externalComponent, "CompositionOutput", "Input");
+
+            composition.ConsolidateConnections();
+        }
+    }
+}
