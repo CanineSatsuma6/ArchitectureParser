8fd0653a1f58495d5164a571c2b84b8156d2044f
Collin McIntyre <crm11396@gmail.com>
2019-12-02 22:02:42 -0600

    Created CompositionFactory class

---
+++ ArchitectureParser/Architecture/Factories/CompositionFactory.cs
@@ -0,0 +1,25 @@
+﻿using ArchitectureParser.Architecture.Components;
+using ArchitectureParser.Architecture.NullObjects;
+
+namespace ArchitectureParser.Architecture.Factories
+{
+    public static class CompositionFactory
+    {
+        public static CompositionBase Create(string name)
+        {
+            CompositionBase composition;
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                composition = new NullComposition();
+            }
+
+            else
+            {
+                composition = new Composition(name);
+            }
+
+            return composition;
+        }
+    }
+}
