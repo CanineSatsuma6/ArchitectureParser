6d0c9572c4b3352ab88b3df384355ef4157d1321
Collin McIntyre <crm11396@gmail.com>
2019-12-02 21:42:13 -0600

    Created NullComposition Class

---
+++ ArchitectureParser/Architecture/Exceptions/NoProvidingSourceException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArchitectureParser.Architecture.Exceptions
+{
+    [Serializable]
+    public class NoProvidingSourceException : Exception
+    {
+        public NoProvidingSourceException(string composition, params string[] neglectedConnections) 
+            : base(string.Format("The following connections on composition \"{0}\" have no source component: {1}", composition, string.Join(", ", neglectedConnections)))
+        {
+
+        }
+
+        public NoProvidingSourceException(string composition, IEnumerable<string> neglectedConnections) 
+            : this(composition, neglectedConnections?.ToArray() ?? new string[0])
+        {
+
+        }
+    }
+}
---
+++ ArchitectureParser/Architecture/NullObjects/NullComposition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+using ArchitectureParser.Architecture.Components;
+using ArchitectureParser.Architecture.Connections;
+
+namespace ArchitectureParser.Architecture.NullObjects
+{
+    public class NullComposition : CompositionBase
+    {
+        private NullSet<Connectable> m_contents;
+        private NullSet<IConnection> m_connections;
+
+        public override ISet<Connectable> Contents
+        {
+            get { return m_contents; }
+        }
+
+        public override ISet<IConnection> Connections
+        {
+            get { return m_connections; }
+        }
+
+        public NullComposition() : base(string.Empty)
+        {
+            m_contents = new NullSet<Connectable>();
+        }
+
+        public override void ConsolidateConnections()
+        {
+            return;
+        }
+    }
+}
