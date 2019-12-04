fa833261d6c0da23bb963873dc5ab3afbb4eea4f
Collin McIntyre <crm11396@gmail.com>
2019-12-04 17:00:04 -0600

    Created ReusableComponentFactory

---
+++ ArchitectureParser/Architecture/Factories/ReusableComponentFactory.cs
@@ -0,0 +1,24 @@
+﻿using ArchitectureParser.Architecture.ReusableComponents;
+
+namespace ArchitectureParser.Architecture.Factories
+{
+    public static class ReusableComponentFactory
+    {
+        public static IReusableComponent Create(string instanceName, string baseName)
+        {
+            IReusableComponent reusableComponent;
+
+            if (string.IsNullOrWhiteSpace(instanceName) || string.IsNullOrWhiteSpace(baseName))
+            {
+                reusableComponent = NullReusableComponent.Instance;
+            }
+
+            else
+            {
+                reusableComponent = new ReusableComponent(instanceName, baseName);
+            }
+
+            return reusableComponent;
+        }
+    }
+}
