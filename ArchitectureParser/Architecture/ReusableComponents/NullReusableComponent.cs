fdffc04908d5629387c69a46aeda772babf13955
Collin McIntyre <crm11396@gmail.com>
2019-12-04 16:50:11 -0600

    Created NullReusableComponent

---
+++ ArchitectureParser/Architecture/ReusableComponents/NullReusableComponent.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+using ArchitectureParser.Architecture.Connections;
+using ArchitectureParser.Architecture.NullObjects;
+
+
+namespace ArchitectureParser.Architecture.ReusableComponents
+{
+    public sealed class NullReusableComponent : IReusableComponent
+    {
+        private static NullReusableComponent m_instance;
+        public  static NullReusableComponent Instance { get { return m_instance ?? (m_instance = new NullReusableComponent()); } }
+
+        private NullSet<IConnection> m_connections;
+
+        public string BaseComponentName
+        {
+            get { return string.Empty; }
+        }
+
+        public string InstanceName
+        {
+            get { return string.Empty; }
+        }
+
+        public ISet<IConnection> Connections
+        {
+            get { return m_connections; }
+        }
+
+        private NullReusableComponent()
+        {
+            m_connections = new NullSet<IConnection>();
+        }
+
+        public IConnection Connect(IConnectable destination, string outputName, string inputName)
+        {
+            return NullConnection.Instance;
+        }
+
+        public override string ToString()
+        {
+            return "Null Reusable Component";
+        }
+    }
+}
